36e0a205b737085d6033d62650c63063d6ac5423
sergeybwk <sergeybwk@gmail.com>
2023-04-27 02:32:44 +0300

    implemnted app and text editor inside it (first version)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
 mod write;
 
-fn main() {}
+struct App {
+    text: String,
+}
+
+impl Default for App {
+    fn default() -> Self {
+        Self {
+            text: "".to_owned(),
+        }
+    }
+}
+
+impl eframe::App for App {
+    fn update(&mut self, ctx: &eframe::egui::Context, frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.centered_and_justified(|ui| {
+                ui.add(egui::TextEdit::multiline(&mut self.text).font(egui::TextStyle::Monospace));
+            })
+        });
+    }
+    fn on_exit(&mut self, _gl: Option<&eframe::glow::Context>) {
+        write::write_to_file(&self.text);
+    }
+}
+
+fn main() -> Result<(), eframe::Error> {
+    let options = eframe::NativeOptions {
+        initial_window_size: Some(egui::vec2(1280.0, 720.0)),
+        resizable: true,
+        ..Default::default()
+    };
+
+    eframe::run_native(
+        "light_texter",
+        options,
+        Box::new(|_cc| (Box::new(App::default()))),
+    )
+    .expect("something wrong with eframe run");
+
+    Ok(())
+}
