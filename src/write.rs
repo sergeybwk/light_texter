90c32e6aacbd4dad4de883afc3be53191537cdee
sergeybwk <sergeybwk@gmail.com>
2023-04-27 01:34:55 +0300

    fixed naming

--- src/write.rs
+++ src/write.rs
@@ -2,13 +2,12 @@ use std::{fs::File, fs::OpenOptions, io::Write};
 
 // Writing the line to the file
 pub fn write_to_file(str: &str) {
-    // let mut file = File::create("text.txt").expect("failed to create the file");
-    let mut filetest = OpenOptions::new()
+    let mut file = OpenOptions::new()
         .create(true)
         .write(true)
         .append(true)
         .open("text.txt")
         .expect("Error dealing with file");
 
-    writeln!(filetest, "{}", str).expect("something is wrong with writing in file");
+    writeln!(file, "{}", str).expect("something is wrong with writing in file");
 }
